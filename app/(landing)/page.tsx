4fa70017e07847dbb99af395d8043d2cdff422e2
Matheus Silva <msilvaqs@outlook.com>
2024-10-02 15:01:25 -0400

    Configuring clerk

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const LandingPage = () => {
-  return <div>LandingPage (Unprotected)</div>;
+  return (
+    <>
+      <div>LandingPage (Unprotected)</div>;
+      <Link href={"/sign-in"}>
+        <Button>Login</Button>
+      </Link>
+      <Link href={"/sign-up"}>
+        <Button>Register</Button>
+      </Link>
+    </>
+  );
 };
 
 export default LandingPage;
