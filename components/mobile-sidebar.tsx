03a074a8d0e543acd1cb7afbaa152726ed927199
Matheus Silva <msilvaqs@outlook.com>
2024-10-02 20:48:25 -0400

    Dashboard page

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Menu } from "lucide-react";
 
 import { Button } from "./ui/button";
@@ -8,6 +8,15 @@ import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./sidebar";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
   return (
     <Sheet>
       <SheetTrigger>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Montserrat } from "next/font/google";
 import {
   Code,
@@ -63,6 +64,8 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -77,7 +80,12 @@ const Sidebar = () => {
         <div className="space-y-1">
           {routes.map((route) => (
             <Link
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
               href={route.href}
               key={route.href}
             >
