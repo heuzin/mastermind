35825e6629d586f27caa73c4bfd8711f0ff4baba
Matheus Silva <msilvaqs@outlook.com>
2024-10-05 18:32:08 -0400

    code page

---
+++ app/api/code/route.ts
@@ -0,0 +1,44 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+
+const client = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
+});
+
+const instructionMessage: ChatCompletionMessageParam = {
+  role: "system",
+  content:
+    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+};
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!client.apiKey) {
+      return new NextResponse("OpenAI API Key not configured", { status: 500 });
+    }
+
+    if (!messages) {
+      return new NextResponse("Messages are required", { status: 400 });
+    }
+
+    const response = await client.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [instructionMessage, ...messages],
+    });
+
+    return NextResponse.json(response.choices[0].message);
+  } catch (error) {
+    console.log("[CODE_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
