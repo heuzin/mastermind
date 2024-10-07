78c1fe4d4cd1009ab6bea65ed025a655cca6f719
Matheus Silva <msilvaqs@outlook.com>
2024-10-07 11:26:17 -0400

    Adding prisma and api calls limit:

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
+import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
+
 const client = new OpenAI({
   apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
 });
@@ -24,11 +26,19 @@ export async function POST(req: Request) {
     if (!resolution)
       return new NextResponse("Resolution is Required", { status: 400 });
 
+    const freeTrial = await checkApiLimit();
+
+    if (!freeTrial) {
+      return new NextResponse("Free trial has expired", { status: 403 });
+    }
+
     const response = await client.images.generate({
       prompt,
       n: parseInt(amount, 10),
       size: resolution,
     });
+
+    await increaseApiLimit();
 
     return NextResponse.json(response.data);
   } catch (error) {
